1cf752ac73f3e60ba795b5e0160b17ee6cc34b97
anirudhRowjee <ani.rowjee@gmail.com>
2021-03-02 19:06:48 +0530

    WIP: Implementing Session
    This commit is a WIP commit implementing the Session object along with
    its tests.

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import random
 import uuid
 import math
 import hashlib
-import cryptography
+import base64
+from cryptography.fernet import Fernet
 
 
 class User:
@@ -17,18 +18,35 @@ class User:
     Represents a user taking part in the chat
     """
 
-    def __init__(self, pre_shared_secret_base, pre_shared_secret_modulus):
+    def __init__(self, name, pre_shared_secret_base, pre_shared_secret_modulus):
         """
+        @param name => name of the user
         @param pre_shared_secret_base must be a large number
         @param pre_shared_secret_modulus must be a large prime number
         """
+        self.name = name
+        self.psk_base = pre_shared_secret_base
+        self.psk_mod = pre_shared_secret_modulus
+
         # DO NOT MAKE THIS ABOVE 100
         self.PRIVATE_KEY_BIT_LIMIT = 2000
         # cryptographically secure random number
         self.private_key = random.getrandbits(self.PRIVATE_KEY_BIT_LIMIT)
         self.public_key = None
-        self.psk_base = pre_shared_secret_base
-        self.psk_mod = pre_shared_secret_modulus
+
+        # set the shared encryption key to avoid redoing expensive computation
+        # TODO this is not good practice. In multi-user situations, this might
+        # not be feasible, but since we are considering a 2-user situation,
+        # this works well.
+        self.shared_secret = None
+        # set the encryption object (Frenet instance)
+        self.encryptor = None
+
+    def get_fernet_key(self, shared_key):
+        # returns the fernet key we can use to encode and decode with fernet
+        # TODO this is breaking, fix it with one of the answers mentioned here
+        # https://stackoverflow.com/questions/44432945/generating-own-key-with-python-fernet
+        return base64.urlsafe_b64encode(bytes(str(shared_key), encoding="utf-8"))
 
     def get_public(self):
         # return the key that will be transported
@@ -45,7 +63,34 @@ class User:
         # not stored as an attribute of self, as we use this to encrypt and
         # decrypt messages
         # return (other_public_key) ** self.private_key % self.psk_mod
-        return pow(other_public_key, self.private_key, self.psk_mod)
+        if not self.shared_secret:
+            self.shared_secret = pow(other_public_key, self.private_key, self.psk_mod)
+        if not self.encryptor:
+            self.encryptor = Fernet(self.get_fernet_key(self.shared_secret))
+        return self.shared_secret
+
+    def encrypt(self, message):
+        # return an encrypted version of a message
+        if not self.encryptor:
+            raise AttributeError(
+                "Encrtyptor is not Defined  - has the shared key been generated?"
+            )
+        else:
+            return self.encryptor.encrypt(message)
+
+    def decrypt(self, ciphertext):
+        # return an encrypted version of a message
+        if not self.encryptor:
+            raise AttributeError(
+                "Encrtyptor is not Defined  - has the shared key been generated?"
+            )
+        else:
+            return self.encryptor.decrypt(ciphertext)
+
+    def send_message(self, message):
+        # functon to return a message object which is to be appended to the
+        # message array, with encryption
+        return {"sender": self.name, "message": self.encrypt(message)}
 
 
 class Session:
@@ -54,18 +99,20 @@ class Session:
     """
 
     def __init__(self):
-        # generate pre-shared secret modulus - this is a mersenne prime for
-        # security, by default will be 2^31 - 1
+
+        # make this a random number that is known to both parties as the common
+        # base
+        self.pss_base = random.getrandbits(200)
+        # a large prime number, hence the 31st Mersenne prime has been chosen
         self.pss_modulus = 2 ** 31 - 1
 
-    def encrypt(self, message, key):
-        pass
+        ## Initialize our users
+        self.user_1 = User(self.pss_base, self.pss_modulus)
+        self.user_2 = User(self.pss_base, self.pss_modulus)
 
-    def decrypt(self, message, key):
-        pass
-
-    def diffie_hellman(self, alice_priv, alice_pub, bob_priv, bob_pub, psk):
-        pass
+        # Message Array - save for the first 2 messages where they key exchange
+        # happens, the rest are encrypted
+        self.messages = []
 
 
 def run_ui():
--- test_user.py
+++ test_user.py
@@ -7,14 +7,14 @@ class TestUser(unittest.TestCase):
         # test the creation of a user
         psk_base = 100
         psk_modulus = 97
-        u1 = User(psk_base, psk_modulus)
+        u1 = User("", psk_base, psk_modulus)
 
     def test_encryption(self):
         # test to make sure both the shared secrets match
         psk_base = 100
         psk_modulus = 2 ** 31 - 1
-        user_1 = User(psk_base, psk_modulus)
-        user_2 = User(psk_base, psk_modulus)
+        user_1 = User("1", psk_base, psk_modulus)
+        user_2 = User("2", psk_base, psk_modulus)
 
         public_user1 = user_1.get_public()
         public_user2 = user_2.get_public()
@@ -23,6 +23,12 @@ class TestUser(unittest.TestCase):
         private_from_user_2 = user_2.get_shared_secret(public_user1)
         # print(private_from_user_2, private_from_user_1)
         self.assertEqual(private_from_user_1, private_from_user_2)
+
+        # now check if the messaging works
+        m1 = user_1.send_message("hello")
+        m2 = user_2.send_message("hello")
+        print(m1, m2)
+        self.assertEqual(m1["message"], m2["message"])
 
 
 if __name__ == "__main__":
